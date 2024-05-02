1c9bf0c714c07a68aa63eb109b4023a9e5be6efd
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-05-02 15:26:46 +0200

    installing plugins

---
+++ lua/plugins/dir-telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "princejoogie/dir-telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    hidden = false,
+    no_ignore = true,
+    show_preview = true,
+  },
+}
+-- Ubuntu requires sudo apt install fd-find
